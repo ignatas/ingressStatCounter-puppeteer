6c47e8ffa618597d86779df9c6110d66bb5e031a
ignatas <40527112+ignatas@users.noreply.github.com>
2019-12-23 23:51:57 +0300

    fail

--- tests/002-login.js
+++ tests/002-login.js
@@ -1,29 +1,53 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const users = {
+    user1: {
+        username: "anyinvalid",
+        password: "anyinvalid",
+        isPositive: false
+    },
+    user2: {
+        username: "' or '1'='1",
+        password: "anyinvalid",
+        isPositive: false
+    },
+    user3: {
+        username: "anyinvalid",
+        password: "anyinvalid",
+        isPositive: true
+    }
+}
 
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 25
-    });
-    //const context = await browser.createIncognitoBrowserContext();
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080
-    });
-    await page.goto('https://aliaksandr-kasko.outsystemscloud.com/AgentProfileStats/', { waitUntil: 'networkidle2' });
+    (async () => {
 
-    //const username = await page.$x('//label[contains(text(), "Username")]/..//input[@type="text"]')
-    //console.log(username)
-    //await page.focus(usernameclear)
-    //await username.click()
-    await page.type('input[type="text"]', '100invalid')
-    await page.type('input[type="password"]', '100invalid')
-    await page.click('input[type="submit"]')
-    await page.waitForXPath('//span[contains(., "Invalid username or password.")]')
-    await page.screenshot({ path: 'screenshots/screenshot.png' });
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 25
+        });
+        const context = await browser.createIncognitoBrowserContext();
 
-    //await context.close();
-    await browser.close();
-})();
+        users.forEach((user) => {
+            const page = await context.newPage();
+            await page.setViewport({
+                width: 1920,
+                height: 1080
+            });
+            await page.goto('https://aliaksandr-kasko.outsystemscloud.com/AgentProfileStats/', { waitUntil: 'networkidle2' });
+
+            //const username = await page.$x('//label[contains(text(), "Username")]/..//input[@type="text"]')
+            //console.log(username)
+            //await page.focus(usernameclear)
+            //await username.click()
+            await page.type('input[type="text"]', user.username)
+            await page.type('input[type="password"]', user.password)
+            await page.click('input[type="submit"]')
+            //await page.waitForXPath('//span[contains(., "Invalid username or password.")]')
+            user.isPositive ? await page.screenshot({ path: `screenshots/screenshot-${user.password}.png` }) : console.log('invalid')
+
+            //await context.close();
+            await page.close();
+
+        })
+
+        await browser.close();
+    })();
